c166505a1abe01ff65d4d9bc0e30dd2f8308eb2c
Kozik Przemysław <Przemyslaw.Kozik@assecods.pl>
2022-05-10 00:56:14 +0200

    SEP-Authentication on the backend side

--- apps/movies-backend/src/main.ts
+++ apps/movies-backend/src/main.ts
@@ -3,16 +3,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('SEP6 - Movie Project')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+    const config = new DocumentBuilder()
+        .setTitle('SEP6 - Movie Project')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+    await app.listen(3000);
 }
 bootstrap();
