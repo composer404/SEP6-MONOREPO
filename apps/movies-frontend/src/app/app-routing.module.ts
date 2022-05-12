78288bba20abf8a2d2bba1e1323a66e7588a8da2
Oliwer Szefer <oszefer@man.poznan.pl>
2022-05-12 22:29:29 +0200

    [SEP] Added the components for the home, login and signup pages. Added the routing for the pages. Imported some fonts and a picture.

--- apps/movies-frontend/src/app/app-routing.module.ts
+++ apps/movies-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeModule} from "./home/home.module";
+import {LoginModule} from "./login/login.module";
+import {SignupModule} from "./signup/signup.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => {
+      return HomeModule;
+    }
+  },
+  {
+    path: 'login',
+    loadChildren: () => {
+      return LoginModule;
+    }
+  },
+  {
+    path: 'signup',
+    loadChildren: () => {
+      return SignupModule;
+    }
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
