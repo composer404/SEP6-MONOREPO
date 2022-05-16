9e05cd0ca3929a50285ac5982269560c98965057
Oliwer Szefer <oszefer@man.poznan.pl>
2022-05-16 21:12:48 +0200

    [SEP] Removed some imports

--- apps/movies-frontend/src/app/core/helpers/error.interceptor.ts
+++ apps/movies-frontend/src/app/core/helpers/error.interceptor.ts
@@ -1,7 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {AuthService} from "../services/auth/auth.service";
-import {Observable, of, throwError} from "rxjs";
+import {Observable, throwError} from "rxjs";
 import {catchError, retry} from 'rxjs/operators';
 
 @Injectable()
@@ -16,10 +15,8 @@ export class ErrorInterceptor implements HttpInterceptor {
         catchError((error: HttpErrorResponse) => {
           let errorMessage = '';
           if (error.error instanceof ErrorEvent) {
-            // client-side error
             errorMessage = `Error: ${error.error.message}`;
           } else {
-            // server-side error
             errorMessage = `Error Status: ${error.status}\nMessage: ${error.message}`;
           }
           console.log(errorMessage);
