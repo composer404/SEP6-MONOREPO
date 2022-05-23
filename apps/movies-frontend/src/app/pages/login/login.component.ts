9a5f45ae011ce7a75a46c425ae152c0a75b12837
Oliwer Szefer <oszefer@man.poznan.pl>
2022-05-23 20:52:00 +0200

    [SEP] Updated the signup and login pages

--- apps/movies-frontend/src/app/pages/login/login.component.ts
+++ apps/movies-frontend/src/app/pages/login/login.component.ts
@@ -3,20 +3,22 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
-import {MessageService} from "primeng/api";
+import {MessageService, PrimeNGConfig} from "primeng/api";
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+    styleUrls: ['./login.component.scss'],
+    providers: [MessageService]
 })
 export class LoginComponent implements OnInit {
     public loginForm: FormGroup;
 
     constructor(private router: Router, private authService: AuthService,
-                private messageService: MessageService) {}
+                private messageService: MessageService, private primengConfig: PrimeNGConfig) {}
 
     ngOnInit(): void {
+      this.primengConfig.ripple = true;
         this.loginForm = new FormGroup({
             login: new FormControl(``, [Validators.required, Validators.minLength(4)]),
             password: new FormControl(``, [Validators.required, Validators.minLength(4)]),
@@ -32,25 +34,25 @@ export class LoginComponent implements OnInit {
         if (token) {
             const profile = await this.authService.getProfile();
             this.router.navigateByUrl(`/board/${profile.id}`);
-            this.addSuccess();
+            this.showSuccess();
             return;
         }
-        this.addError();
+        this.showError();
     }
 
-  public clear(): void {
-    this.messageService.clear();
-  }
+    public clear() {
+        this.messageService.clear();
+    }
 
     onClickSignUp(): void {
         this.router.navigateByUrl('/signup');
     }
 
-    private addSuccess(): void {
-      this.messageService.add({severity:'success', summary:'Success', detail:'You have been logged in properly'});
+    showSuccess() {
+        this.messageService.add({severity:'success', summary:'Success', detail:'You have been logged in properly'});
     }
 
-    private addError(): void {
-      this.messageService.add({severity:'error', summary:'Error', detail:'You have not been logged in properly'});
+    showError() {
+        this.messageService.add({severity:'error', summary:'Error', detail:'You have not been logged in properly'});
     }
 }
