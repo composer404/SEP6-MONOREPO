bcb38a66e9c44d069afcc1bd37cfaa66df1a1d8c
Kozik Przemysław <Przemyslaw.Kozik@assecods.pl>
2022-05-28 19:36:22 +0200

    update

--- apps/movies-backend/src/app.module.ts
+++ apps/movies-backend/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { UsersModule } from './modules/users/';
 import { AuthModule } from './modules/auth/';
 import { MoviesModule } from './modules/movies/';
 import { TopListsModule } from './modules/top-lists';
 import { RatingsModule } from './modules/ratings';
 import { CommentsModule } from './modules/comments';
+import { exec } from 'child_process';
 
 @Module({
     imports: [UsersModule, AuthModule, MoviesModule, TopListsModule, RatingsModule, CommentsModule],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+    onModuleInit() {
+        exec(`npx prisma migrate deploy`);
+    }
+}
